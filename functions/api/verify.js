ed42933dc790da26fdb67c21611686c6fe71ae11
Vivek Nair <vivek@nair.me>
2022-06-09 17:46:32 -0700

    add email function

---
+++ functions/api/verify.js
@@ -0,0 +1,25 @@
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
+export async function onRequest(context) {
+  if(context.request.method == "POST") {
+    const { searchParams } = new URL(request.url);
+    const email = searchParams.get('email');
+    if (typeof email !== 'string' || email.length === 0) {
+      return new Response("Expected email");
+    } else if (!validateEmail(email)) {
+      return new Response("Invalid email");
+    } else {
+      const input = email;
+      const digest = await crypto.subtle.digest({name: 'SHA-256'}, input);
+      const string = new TextDecoder().decode(digest);
+      const base64 = btoa(string);
+      return new Response(btoa);
+    }
+  } else {
+    return new Response("Expected POST");
+  }
+}
